66e21ed915f5e7e37a129713c008eb437ed41954
Farhath Ayesha <farhath.ayesha@appnetwise.com>
2025-09-08 18:54:00 +0530

    updated config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/farhath-portfolio/",
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
@@ -55,7 +56,7 @@ export default defineConfig({
   },
   build: {
     target: "esnext",
-    outDir: "build",
+    outDir: "dist",
   },
   server: {
     port: 8000,
